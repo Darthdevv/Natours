56e3763b564919b63ecc85711fd44a7a575167a4
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-04-12 03:49:31 +0200

    🪴 feat: Aggregation Pipeline: Matching and Grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -91,3 +91,41 @@ exports.createTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRating: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'DIFFICULT' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      data: { stats },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
+};
